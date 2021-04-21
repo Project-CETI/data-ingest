8dd90133a2a93e9d5566dca23533982c080c6eaa
Mykola Pavlov <me@nikolaypavlov.com>
2021-04-21 16:37:07 +0300

    Fix missing args

--- ceti/s3upload.py
+++ ceti/s3upload.py
@@ -19,10 +19,11 @@ INGESTION_DATE_UTC = datetime.now(timezone.utc).strftime('%Y-%m-%d')
 def s3object_exists(s3client, bucket_name, s3_key):
 
     try:
-        results = client.list_objects(Bucket=bucket_name, Prefix=s3_key)
+        results = s3client.list_objects(Bucket=bucket_name, Prefix=s3_key)
         if ('Contents' in results):
             return True
-    except:
+
+    except Exception:
         # Here, something else could have gone wrong,
         # but for safety we will assume that the file is not present in S3
         pass
@@ -54,8 +55,10 @@ def to_s3_key(data_dir: str, src: Path) -> Path:
     key_path = Path('raw') / INGESTION_DATE_UTC / src.relative_to(data_dir)
     return key_path
 
-def to_hash_key(data_dir: str, src: Path) -> Path:
+
+def to_hash_key(src: Path) -> Path:
     """Create S3 key corresponding to hash of the local file contents"""
+
     key_path = Path('raw') / "hash" / str(sha256sum(src))
     return key_path
 
@@ -65,7 +68,7 @@ def upload_files(s3client, data_dir: str, filelist: Sequence[Path]) -> None:
 
     for src in filelist:
         s3_key = str(to_s3_key(data_dir, src))
-        s3_hash_key = str(to_hash_key(data_dir, src))
+        s3_hash_key = str(to_hash_key(src))
         local_path = str(src.resolve())
 
         with create_progress_bar(src) as progress:
@@ -78,6 +81,7 @@ def upload_files(s3client, data_dir: str, filelist: Sequence[Path]) -> None:
                 fp.flush()
                 s3client.upload_file(local_path, BUCKET_NAME, s3_key, Callback=progress.update)
                 s3client.upload_file(fp.name, BUCKET_NAME, s3_hash_key)
+
 
 def cli(args: Namespace):
     files = get_filelist(args.data_directory)
