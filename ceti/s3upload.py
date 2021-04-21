a669b37238e26764ee125e4440eee680f9f80be6
Peter Malkin <github.com/PeterMalkin>
2021-04-21 15:40:53 -0700

    Rework the deduplication algorithm to name the files in s3 buckets using their sha256 hash as the name of the file.

--- ceti/s3upload.py
+++ ceti/s3upload.py
@@ -2,7 +2,6 @@ from argparse import Namespace
 from datetime import datetime, timezone
 from pathlib import Path
 import re
-import tempfile
 from typing import Sequence
 
 import boto3
@@ -17,18 +16,13 @@ MAX_CONCURRENCY = 20
 INGESTION_DATE_UTC = datetime.now(timezone.utc).strftime('%Y-%m-%d')
 
 
-def s3object_exists(s3client, bucket_name: str, s3_key: str) -> bool:
-
+def get_s3_filenames(s3client, bucket_name: str) -> list:
+    hashes = list()
     try:
-        results = s3client.list_objects(Bucket=bucket_name, Prefix=s3_key)
-        if ('Contents' in results):
-            return True
-
-    except Exception:
-        # Here, something else could have gone wrong,
-        # but for safety we will assume that the file is not present in S3
-        pass
-    return False
+        for key in s3client.list_objects(Bucket='bucket_name')['Contents']:
+            hashes.append(Path(key['Key']).stem)
+    finally:
+        return hashes
 
 
 def create_progress_bar(file_path: Path) -> tqdm:
@@ -50,62 +44,55 @@ def get_filelist(src_dir: str) -> Sequence[Path]:
     return [p for p in sorted(path.glob('**/*')) if p.is_file()]
 
 
-def to_s3_key(data_dir: str, src: Path) -> Path:
+def to_s3_key(data_dir: str, src: Path, s3filenames: list) -> Path:
     """Create S3 key to address the file in the bucket"""
 
-    local_file_path = str(src.relative_to(data_dir))
-    if not re.match(r".+\/.+", local_file_path):
+    filename = str(src.relative_to(data_dir))
+    print(f"hashing {src}")
+    hash = str(sha256sum(str(src.resolve())))
+    if hash in s3filenames:
+        return None
+    filename = filename.replace(src.stem, hash)
+
+    if not re.match(r".+\/.+", filename):
         # The file we are trying to upload is not
         # in the local folder that defines the device
         # that captured this file.
         # Therefore, it should be uploaded into
         # the "/unknown-device/" folder in S3
-        local_file_path = "unknown-device/"+local_file_path
+        filename = "unknown-device/"+filename
 
-    key_path = Path('raw') / INGESTION_DATE_UTC / local_file_path
+    key_path = Path('raw') / INGESTION_DATE_UTC / filename
     return key_path
 
 
-def to_hash_key(src: Path) -> Path:
-    """Create S3 key corresponding to hash of the local file contents"""
-
-    key_path = Path('raw') / "hash" / str(sha256sum(src))
-    return key_path
-
-
-def upload_files(s3client, data_dir: str, filelist: Sequence[Path]) -> None:
+def upload_files(s3client, data_dir: str, filelist: Sequence[Path], s3filenames: list) -> None:
     """Execute file upload procedure"""
 
     for src in filelist:
-        s3_key = str(to_s3_key(data_dir, src))
-        s3_hash_key = str(to_hash_key(src))
+        s3_key = str(to_s3_key(data_dir, src, s3filenames))
+        if not s3_key:
+            continue
         local_path = str(src.resolve())
 
         with create_progress_bar(src) as progress:
-
-            if (s3object_exists(s3client, BUCKET_NAME, s3_hash_key)):
-                continue
-
-            with tempfile.NamedTemporaryFile(mode="wt") as fp:
-                fp.write(s3_key)
-                fp.flush()
-                s3client.upload_file(local_path, BUCKET_NAME, s3_key, Callback=progress.update)
-                s3client.upload_file(fp.name, BUCKET_NAME, s3_hash_key)
+            s3client.upload_file(local_path, BUCKET_NAME, s3_key, Callback=progress.update)
 
 
 def cli(args: Namespace):
+
     files = get_filelist(args.data_directory)
+    botocore_config = botocore.config.Config(max_pool_connections=MAX_CONCURRENCY)
+    s3client = boto3.client('s3', config=botocore_config)
+    s3filenames = get_s3_filenames(s3client, BUCKET_NAME)
 
     if args.debug:
         boto3.set_stream_logger('')
 
     if args.dry_run:
         for src in files:
-            dst = to_s3_key(args.data_directory, src)
+            dst = to_s3_key(args.data_directory, src, s3filenames)
             print(f"{src} -> s3://{BUCKET_NAME}/{dst}")
 
     else:
-        botocore_config = botocore.config.Config(max_pool_connections=MAX_CONCURRENCY)
-        s3client = boto3.client('s3', config=botocore_config)
-
-        upload_files(s3client, args.data_directory, files)
+        upload_files(s3client, args.data_directory, files, s3filenames)
