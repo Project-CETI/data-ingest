21fca932b403b3b94d9bb2d8f4eede44342eb051
Mykola Pavlov <me@nikolaypavlov.com>
2021-06-15 12:53:34 +0300

    S3 upload test

--- tests/test_s3upload.py
+++ tests/test_s3upload.py
@@ -1,4 +1,8 @@
 from pathlib import Path
+import shutil
+import tempfile
+
+import boto3
 
 from ceti import s3upload
 
@@ -11,3 +15,12 @@ def test_get_filelist():
 
     for f in TEST_FILES:
         assert f in files
+
+
+def test_file_upload():
+    with tempfile.TemporaryDirectory() as dst_dir:
+        shutil.copytree(TEST_DATA_DIR, dst_dir)
+
+        files = s3upload.get_filelist(dst_dir)
+        s3client = boto3.client('s3')
+        s3upload.sync_files(s3client, dst_dir, files)
