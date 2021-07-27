7513237ad318280070c5c4409e5bce2ae6e2c33a
Mykola Pavlov <me@nikolaypavlov.com>
2021-07-27 10:05:26 +0300

    Bump version: 1.0.12 → 1.0.13

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description='CLI data ingestion tools for Project CETI',
     author='Nikolay Pavlov, Peter Malkin',
     author_email='me@nikolaypavlov.com, petermalkin@google.com',
-    version="1.0.12",
+    version="1.0.13",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
