df6dd1b0c20bd722927bf2ee020f7c2f0f5e7243
Peter Malkin <github.com/PeterMalkin>
2021-11-03 17:29:00 -0700

    Modify HelloWorld PySpark job to write to S3://ceti-dev-test

--- ceti/spark/jobs/helloworld.py
+++ ceti/spark/jobs/helloworld.py
@@ -1,15 +1,22 @@
 # This file contains a sample PySpark application
 # to be run on CETI EMR cluster for test purposes.
 
+import logging
 from pyspark.sql import SparkSession
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+
+OUTPUT_BUCKET = "s3://ceti-data-test"
 
 def helloworld():
     spark = SparkSession.builder.appName("HelloWorld").getOrCreate()
-    nums = spark.sparkContext.parallelize(["Hello", "World"])
-    print(nums.map(lambda x: x).collect())
+    rdd = spark.sparkContext.parallelize(["Hello", "World"])
+    res = rdd.map(lambda x: x).collect()
+    print(res)
+    logger.info("%s", res)
+    rdd.coalesce(1).saveAsTextFile(f"{OUTPUT_BUCKET}/HelloWorld.txt")
     spark.stop()
-
 
 if __name__ == "__main__":
     helloworld()
