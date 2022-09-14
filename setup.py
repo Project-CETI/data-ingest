3c0a9f6e92bb93dc331c950a825aa91b4ee3d06b
Peter Malkin <github.com/PeterMalkin>
2022-09-13 22:00:32 -0700

    Bump version: 1.0.18 → 1.0.19

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description='CLI data ingestion tools for Project CETI',
     author='Nikolay Pavlov, Peter Malkin',
     author_email='me@nikolaypavlov.com, petermalkin@google.com',
-    version="1.0.18",
+    version="1.0.19",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
