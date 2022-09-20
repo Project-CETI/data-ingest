c53045cab8ae1e3af4556c2ccb3844a10f46aabe
PhillipShahviri <phillipshahviri@gmail.com>
2022-09-20 08:54:55 -0400

    Added multiple supported video formats

--- ceti/video_offload.py
+++ ceti/video_offload.py
@@ -4,12 +4,15 @@ import time
 import shutil
 
 LOCAL_VIDEO_DATA = os.path.join(os.getcwd(), "video_data")
+SUPPORTED_FILE_EXTENSIONS = ["mp4", "mov", "mks", "avi"]
 
 def get_video_files(data_directory):
     files = os.listdir(data_directory)
     video_files = []
     for file in files:
-        if file.lower().endswith(".mp4"):
+        filepath = os.path.join(data_directory, file)
+        ext = os.path.splitext(filepath)[1][1:].lower() #Extract the file extension
+        if ext in SUPPORTED_FILE_EXTENSIONS:
             video_files.append(file)
     return video_files
 
@@ -26,11 +29,12 @@ def offload_files(files_to_offload, data_directory):
 
         #Get the epoch time of when the file was created
         storage_device_filepath = os.path.join(data_directory, file)
+        ext = os.path.splitext(storage_device_filepath)[1][1:] #Extract the file extension
         epoch_time = int(os.path.getctime(storage_device_filepath))
-        epoch_name = f'{epoch_time}.MP4'
+        epoch_name = f'{epoch_time}.{ext}'
 
         #Copy and rename file if it is not in directory already
-        if not epoch_name  in local_files:
+        if not epoch_name in local_files:
             shutil.copy(os.path.join(storage_device_filepath), LOCAL_VIDEO_DATA)
             os.rename(os.path.join(LOCAL_VIDEO_DATA, file), os.path.join(LOCAL_VIDEO_DATA, epoch_name))
             print(f'{file} -> {epoch_name}')
